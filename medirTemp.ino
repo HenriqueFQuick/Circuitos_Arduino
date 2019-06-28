d4f071f3ab1b5a169d75b6ded7f5a6446db55015
Henrique Fernandes <henrique.fquick@gmail.com>
2019-06-28 15:15:46 -0300

    Add files via upload

---
+++ medirTemp.ino
@@ -0,0 +1,15 @@
+#define pot A0
+void setup() {
+  Serial.begin(9600);
+  pinMode(pot, INPUT);
+}
+
+void loop() {
+  int val = analogRead(pot);
+  float temperatura = (val-512)*2.4;  
+  Serial.print("Potenciometro: ");
+  Serial.print(val);
+  Serial.print("   Temperatura: ");
+  Serial.println(temperatura);
+  delay(500);
+}
